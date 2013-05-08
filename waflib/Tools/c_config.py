393491821789a59f72bb76d1e292eae357a6a125
Thomas Nagy <tnagy2pow10@gmail.com>
2013-05-08 23:18:53 +0200

    Issue 1253

--- waflib/Tools/c_config.py
+++ waflib/Tools/c_config.py
@@ -1095,7 +1095,7 @@ def get_cc_version(conf, cc, gcc=False, icc=False):
 			conf.env.DEST_BINFMT = 'elf'
 		elif isD('__WINNT__') or isD('__CYGWIN__'):
 			conf.env.DEST_BINFMT = 'pe'
-			conf.env.LIBDIR = conf.env['PREFIX'] + '/bin'
+			conf.env.LIBDIR = conf.env.BINDIR
 		elif isD('__APPLE__'):
 			conf.env.DEST_BINFMT = 'mac-o'
 
