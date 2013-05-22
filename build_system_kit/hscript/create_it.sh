f7b604184a8edbbcac71b709aba034861c4d0548
Sebastien Binet <binet@cern.ch>
2013-05-22 09:15:49 +0200

    hscript: properly generate h-waf binary

--- build_system_kit/hscript/create_it.sh
+++ build_system_kit/hscript/create_it.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 D=$PWD
+TOOLS="$D/hlib.py,batched_cc,compat,compat15,ocaml,go,cython,scala,erlang,cuda,gcj,boost,pep8,subprocess,parallel_debug"
+
 pushd ../..
-./waf-light configure build --tools=$D/hlib.py --prelude=$'\tfrom waflib.extras import hlib'
+./waf-light configure build --tools=$TOOLS --prelude=$'\tfrom waflib.extras import hlib'
 popd
 cp ../../waf h-waf
 
