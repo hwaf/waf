578816dbca5120d572e4596f7594f8781c8180ff
Thomas Nagy <tnagy2pow10@gmail.com>
2013-09-04 00:20:42 +0200

    Issue 1335 - regression in the library installation

--- waflib/Tools/ccroot.py
+++ waflib/Tools/ccroot.py
@@ -511,7 +511,8 @@ def apply_vnum(self):
 		bld = self.bld
 		path = self.install_task.dest
 		if self.env.DEST_OS == 'openbsd':
-			t1 = bld.install_as('%s%s%s' % (path, os.sep, name3), node, env=self.env, chmod=self.link_task.chmod)
+			libname = self.outputs[0].name
+			t1 = bld.install_as('%s%s%s' % (path, os.sep, libname), node, env=self.env, chmod=self.link_task.chmod)
 			self.vnum_install_task = (t1,)
 		else:
 			t1 = bld.install_as(path + os.sep + name3, node, env=self.env, chmod=self.link_task.chmod)
