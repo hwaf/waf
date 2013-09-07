daa91dba4b881d86bc25eec90a3745ebaeece835
Thomas Nagy <tnagy2pow10@gmail.com>
2013-09-07 10:33:33 +0200

    Issue 1356 icl detection with python 3

--- waflib/Tools/msvc.py
+++ waflib/Tools/msvc.py
@@ -496,10 +496,10 @@ def gather_intel_composer_versions(conf, versions):
 					setattr(conf, compilervars_warning_attr, False)
 					patch_url = 'http://software.intel.com/en-us/forums/topic/328487'
 					compilervars_arch = os.path.join(path, 'bin', 'compilervars_arch.bat')
-					for vscomntools in ['VS110COMNTOOLS', 'VS100COMNTOOLS']:
-						if os.environ.has_key (vscomntools):
-							vs_express_path = os.environ[vscomntools] + r'..\IDE\VSWinExpress.exe'
-							dev_env_path = os.environ[vscomntools] + r'..\IDE\devenv.exe'
+					for vscomntool in ['VS110COMNTOOLS', 'VS100COMNTOOLS']:
+						if vscomntool in os.environ:
+							vs_express_path = os.environ[vscomntool] + r'..\IDE\VSWinExpress.exe'
+							dev_env_path = os.environ[vscomntool] + r'..\IDE\devenv.exe'
 							if (r'if exist "%VS110COMNTOOLS%..\IDE\VSWinExpress.exe"' in Utils.readf(compilervars_arch) and
 								not os.path.exists(vs_express_path) and not os.path.exists(dev_env_path)):
 								Logs.warn(('The Intel compilervar_arch.bat only checks for one Visual Studio SKU '
